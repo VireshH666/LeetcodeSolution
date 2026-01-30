b7b1b5d4dc73947c68913eb83e99cd1d3db57f76
VireshH666 <vireshhunasagi134@gmail.com>
2026-01-30 21:22:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        big=nums[0]
+        ct=1
+        for i in range(1,len(nums)):
+            if big==nums[i]:
+                ct+=1
+            else:
+                ct-=1   
+                if ct==0:
+                    big=nums[i] 
+                    
+        return big                
+        
